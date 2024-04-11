a4c3a0e84368532f68452953ec58d44cc7e38364
emilyrhee <emilyrhee99@gmail.com>
2024-04-11 18:56:22 -0400

    added isPrime function

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+char isPrime(int number) {
+    for (int i = 2; i < number; i++) { // every number is divisible by 1, so start at 2
+        if (number % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
 int main() {
+    int number = 20;
+
+    if (isPrime(number)) {
+        printf("%d is prime\n", number);
+    } else {
+        printf("%d is not prime\n", number);
+    }
+
     return 0;
 }
