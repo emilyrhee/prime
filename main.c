4c892b6df643d85006d0a0d25c11a1deedb1835f
emilyrhee <emilyrhee99@gmail.com>
2024-04-15 16:59:38 -0400

    added character input

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void setNonCanonicalMode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void setCanonicalMode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= ICANON | ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
 
 char isPrime(int number) {
     for (int i = 2; i < number; i++) { // every number is divisible by 1, so start at 2
@@ -10,16 +25,26 @@ char isPrime(int number) {
 }
 
 int main() {
-    int number = 2;
+    int number = 2; // 2 is the first prime number
+    setNonCanonicalMode();
 
-    while (number < 200) {
+    while (1) {
         if (isPrime(number)) {
-            printf("%d\n", number);
-            sleep(1);
+            printf("%-8d    Quit [y/n]? ", number);
+            fflush(stdout);
+
+            char input;
+            read(STDIN_FILENO, &input, 1);
+            printf("%c\n", input);
+            if (input == 'y' || input == 'Y') {
+                setCanonicalMode();
+                return 0;
+            }
         }
 
         number++;
     }
 
+    setCanonicalMode();
     return 0;
 }
